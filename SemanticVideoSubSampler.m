0e9ecc32a02f48f1e490d84fe8395956aff7a374
Alan <alan.neves@dcc.ufmg.br>
2019-10-24 18:34:37 -0300

    - Video matrix limits test

--- SemanticVideoSubSampler.m
+++ SemanticVideoSubSampler.m
@@ -183,6 +183,9 @@ classdef SemanticVideoSubSampler < handle
             %for i = 1:length(frame_indices)
             i = 1; z = 0;
             while hasFrame(reader)
+                if (i > size(frame_indices,2))
+                    break;
+                end
                 %frame = read(reader,frame_indices(i));
                 frame = readFrame(reader);
                 z = z + 1;
